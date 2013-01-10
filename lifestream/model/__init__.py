a86f16109a9cada105eab71e4adfa73306ba1228
Billy <billy@dreammx.com>
2013-01-10 21:23:14 +0800

    Clean up

--- lifestream/model/__init__.py
+++ lifestream/model/__init__.py
@@ -8,3 +8,30 @@ class Stream(db.Expando):
 	origin = db.StringProperty(required=True, indexed=False)
 	subject = db.StringProperty(required=True)
 	link = db.StringProperty(required=True)
+
+class Counter(db.Model):
+	key = db.StringProperty(required=True, indexed=True)
+	value = db.IntegerProperty(required=True, indexed=True)
+
+	@staticmethod
+	def add(key, value):
+		counter = Counter(key, value)
+		return counter.put()
+
+	@staticmethod
+	def update(key, value):
+		counter = Counter(key, value)
+		return counter.save()
+
+	@staticmethod
+	def delete(key, value):
+		counter = Counter.get(key)
+		counter.delete()
+	
+	def incr(value = 1):
+		self.value = self.value + value
+		return self.save()
+
+	def decr(value = 1):
+		self.value = self.value - value
+		return self.save()
